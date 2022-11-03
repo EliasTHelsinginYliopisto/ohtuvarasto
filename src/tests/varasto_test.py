d902d701828e0237c005c79655b68f83961253e8
Elias Toukolehto <elias.toukolehto@helsinki.fi>
2022-11-03 18:52:11 +0200

    vk1 tehtava 8

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,46 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisaa_negatiivinen_maara(self):
+        self.varasto.lisaa_varastoon(-2)
+
+        #ei voi lisää negatiivista lukua
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 10)
+    
+    def test_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(20)
+
+        #ei voi lisää liikaa
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+
+    def test_virheellinen_varasto_koko(self):
+        varasto = Varasto(-10)
+
+        #varaston tila on luvun itseisarvo
+        self.assertAlmostEqual(varasto.paljonko_mahtuu(),10)
+    
+    def test_virheellinen_varasto_saldo(self):
+        varasto = Varasto(10, alku_saldo=-10)
+
+        #negatiivinen saldo muuttuu nollaksi
+        self.assertAlmostEqual(varasto.paljonko_mahtuu(),10)
+
+    def test_ota_virheellisesti(self):
+        
+        #negatiivisilla luvuilla ei oteta
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-5),0)
+
+    def test_ota_liikaa(self):
+        self.varasto.lisaa_varastoon(5)
+
+        #otetaan kaikki mitä voidaan
+        self.assertAlmostEqual(self.varasto.ota_varastosta(100),5)
+    
+    def test_str_toimii(self):
+        output = "saldo = 0, vielä tilaa 10"
+
+        self.assertEqual(self.varasto.__str__(),output )
+
+
+    
